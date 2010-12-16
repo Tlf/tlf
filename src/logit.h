57625f9b0283baea06be610718c96a5f9c824f56
Thomas Beierlein <tb@forth-ev.de>
2010-12-16 10:09:13 +0000

    simplify include files

--- src/logit.h
+++ src/logit.h
@@ -16,10 +16,8 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <curses.h>
+
+#include "tlf.h"
 #include "clear_display.h"
 #include "printcall.h"
 #include "callinput.h"
@@ -29,7 +27,6 @@
 #include "log_to_disk.h"
 #include "keyer.h"
 #include "recall_exchange.h"
-#include "tlf.h"
 #include "sendqrg.h"
 #include "lancode.h"
 
