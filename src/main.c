Tlf/tlf

commit 671e75d0ec036df8a03bbc5315197c6e87d9f360

add recognition of new keyowrd CABRILLO=
That keyword has to be used to name the cabrillo format definition to be
used when writing cabrillo logs.

src/main.c
@@ -167,6 +167,7 @@ int defer_store = 0;
 char buffer[162];
 char call[20];
 char logfile[120] = "general.log";
+char *cabrillo;			/*< Name of the cabrillo format definition */
 char synclogfile[120];
 char markerfile[120] = "";
 int xplanet = 0;

src/parse_logcfg.c
@@ -41,6 +41,7 @@ extern int portnum;
 extern int packetinterface;
 extern int tncport;
 extern int shortqsonr;
+extern char * cabrillo;
 
 int exist_in_country_list();
 
@@ -49,7 +50,7 @@ FILE *fp;
 
 void KeywordNotSupported(char *keyword);
 
-#define  MAX_COMMANDS 157	/* commands in list */
+#define  MAX_COMMANDS 158	/* commands in list */
 
 
 int read_logcfg(void)
@@ -71,6 +72,7 @@ int read_logcfg(void)
     nodes = 0;
     node = 0;
     shortqsonr = 0;
+    cabrillo = NULL;
 
     strcpy(defltconf, PACKAGE_DATA_DIR);
     strcat(defltconf, "/logcfg.dat");
@@ -388,8 +390,9 @@ void parse_logcfg(char *inputbuffer)
 	"DIGIMODEM=",
 	"LOGFREQUENCY",
 	"IGNOREDUPE",
-	"CW_TU_MSG=",				/* deprecated */
-	"VKCWR=",		/* 155 */	/* deprecated */
+	"CABRILLO=",
+	"CW_TU_MSG=",		/* 155 */	/* deprecated */
+	"VKCWR=",				/* deprecated */
 	"VKSPR="				/* deprecated */
     };
 
@@ -1295,9 +1298,13 @@ void parse_logcfg(char *inputbuffer)
 		ignoredupe = 1;
 		break;
 	    }
-    case 154:
+    case 154:{		/* read name of cabrillo format to use */
+	    	cabrillo = strdup(g_strchomp(inputbuffer + 9));
+    		break;
+	    }
     case 155:
-    case 156: {
+    case 156:
+    case 157:{
 		KeywordNotSupported(teststring);
 		break;
 	    }

commit 2f5abdf4b6a9c91b9b0e7d478ea076cdfe38ea0e

Fix possible memory leak introduced with commit 671e75d0ec
* Can happen if
  - rereading config file
  - CABRILLO= defined more than once

src/main.c
@@ -167,7 +167,7 @@ int defer_store = 0;
 char buffer[162];
 char call[20];
 char logfile[120] = "general.log";
-char *cabrillo;			/*< Name of the cabrillo format definition */
+char *cabrillo = NULL;		/*< Name of the cabrillo format definition */
 char synclogfile[120];
 char markerfile[120] = "";
 int xplanet = 0;

src/parse_logcfg.c
@@ -72,7 +72,11 @@ int read_logcfg(void)
     nodes = 0;
     node = 0;
     shortqsonr = 0;
-    cabrillo = NULL;
+    
+    if (cabrillo != NULL) {
+	free(cabrillo);
+	cabrillo = NULL;
+    }
 
     strcpy(defltconf, PACKAGE_DATA_DIR);
     strcat(defltconf, "/logcfg.dat");
@@ -1299,6 +1303,11 @@ void parse_logcfg(char *inputbuffer)
 		break;
 	    }
     case 154:{		/* read name of cabrillo format to use */
+
+		if (cabrillo != NULL) {
+		    free(cabrillo);	/* free old string if already set */
+		    cabrillo = NULL;
+		}
 	    	cabrillo = strdup(g_strchomp(inputbuffer + 9));
     		break;
 	    }