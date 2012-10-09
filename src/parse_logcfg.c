a4d0c846a560a3612c42463dd77dd3f5588c16f8
Thomas Beierlein <tb@forth-ev.de>
2012-10-09 11:07:57 +0200

    Correct wording for some error message

--- src/parse_logcfg.c
+++ src/parse_logcfg.c
@@ -1472,7 +1472,7 @@ void Complain(char *msg) {
 void KeywordNotSupported(char *keyword) {
     char msgbuffer[100];
     sprintf(msgbuffer,
-	    "Keyword '%s' not supported. See man page and README.\n",
+	    "Keyword '%s' not supported. See man page.\n",
 	    keyword);
     Complain(msgbuffer);
 }
