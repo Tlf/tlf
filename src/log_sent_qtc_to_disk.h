1e1ab603d06f3a2e1fd7dd4c9c6c941a95a9fb62
airween <airween@gmail.com>
2013-11-01 21:09:03 +0100

    QTC receive modul: QTC lines window.

--- src/log_sent_qtc_to_disk.h
+++ src/log_sent_qtc_to_disk.h
@@ -1,7 +1,8 @@
 /*
  * Tlf - contest logging program for amateur radio operators
- * Copyright (C) 2001-2002-2003 Rein Couperus <pa0rct@amsat.org>
+ * Copyright (C) 2013           Ervin Hegedüs - HA2OS <airween@gmail.com>
  *
+ * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
