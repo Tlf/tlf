d7a0bc3be8d76caf947060101636b7291b8582de
Thomas Beierlein <tomjbe@gentoo.org>
2013-12-27 16:22:51 +0100

    drop unneeded statements
    - leftover from Rein's old code
    - NDEBUG controls 'assert()'s behaviour, but has to be defined for every
      file. so it was on the wrong place anyway.

--- src/main.c
+++ src/main.c
@@ -17,8 +17,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-#define NDEBUG
-#define NEWCODE = 1
 
 #include "tlf.h"
 #include "globalvars.h"
