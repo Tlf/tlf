21b2df0f595bc18c4c591426779cdb4fab4914ee
airween <airween@gmail.com>
2013-12-28 21:42:04 +0100

    Remove syslog.h header

--- src/score.c
+++ src/score.c
@@ -3,6 +3,32 @@
  * Copyright (C) 2001-2002-2003 Rein Couperus <pa0rct@amsat.org>
  *               2013           Ervin Hegedus <airween@gmail.com>
  *
+ * the calc_qrb() function original code in HAMLIB
+ * src/locator.c
+ * 
+ * author Stephane Fillod and the Hamlib Group
+ * date 2000-2010
+ *
+ *  Hamlib Interface - locator, bearing, and conversion calls
+ *
+ *
+ *  Hamlib Interface - locator and bearing conversion calls
+ *  Copyright (c) 2001-2010 by Stephane Fillod
+ *  Copyright (c) 2003 by Nate Bargmann
+ *  Copyright (c) 2003 by Dave Hines
+ *
+ *
+ *  Code to determine bearing and range was taken from the Great Circle,
+ *  by S. R. Sampson, N5OWK.
+ *  Ref: "Air Navigation", Air Force Manual 51-40, 1 February 1987
+ *  Ref: "ARRL Satellite Experimenters Handbook", August 1990
+ *
+ *  Code to calculate distance and azimuth between two Maidenhead locators,
+ *  taken from wwl, by IK0ZSN Mirko Caserta.
+ *
+ *  New bearing code added by N0NB was found at:
+ *  http://williams.best.vwh.net/avform.htm#Crs
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -25,8 +51,6 @@
 #include "qrb.h"
 #include "locator2longlat.h"
 #include "score.h"
-
-#include "syslog.h"
 
 #define RADIAN  (180.0 / M_PI)
 #define ARC_IN_KM 111.2
