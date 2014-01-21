ea95d4d8eb0adfdec759ba928b333079f5d1cf88
Thomas Beierlein <tomjbe@gentoo.org>
2014-01-21 21:15:23 +0100

    fix scoring for FOC marathon
    1 point per QSO
    2 points per country
    5 points per continent
    5b qso gets 10 points bonus
    6b qso gets 15 points bonus
    Gx4FOC joker counts double

--- src/foc.c
+++ src/foc.c
@@ -30,16 +30,18 @@ extern int searchflg;
 extern int total;
 extern int countries[];
 
-int got_g4foc; 		/* did we got Gx4FOC on the air? */
+int g4foc_index; 		/* index of Gx4FOC in callarray */
+int g4foc_count; 		/* how often did we work him */
 int five_banders;
 int six_banders;
+int cntry;
+int cont;
 
 void count_56_banders();
 
 /** Initialize settings for FOC contest */
 void foc_init(void) {
     foc = 1;
-    got_g4foc = 0;
     contest = 1;
     showscore_flag = 1;
     searchflg = 1;
@@ -59,7 +61,6 @@ int foc_score(char *call) {
 
     if (g_regex_match_simple("^G(|[A-Z])4FOC(|/.*)", call,
 	    G_REGEX_CASELESS, 0)) {
-	got_g4foc = 1;
 	return 2;
     }
     else {
@@ -83,7 +84,7 @@ int nr_of_bands(int x) {
 
 /** count 5/6banders
  *
- * count the number of stations worked on 5 or 6 band
+ * count the number of stations worked on 5 or 6 band (including G4FOC)
  */
 void count_56_banders() {
     int i, nr;
@@ -104,10 +105,39 @@ void count_56_banders() {
 }
 
 
+int search_g4foc_in_callarray(void) {
+    extern int callarray_nr;
+    extern char callarray[MAX_CALLS][20];
+
+    int found = -1;
+    int i;
+
+    for (i = 0; i < callarray_nr; i++) {
+
+	if (g_regex_match_simple("^G(|[A-Z])4FOC(|/.*)", callarray[i],
+		G_REGEX_CASELESS, 0)) {
+            found = i;
+            break;
+        }
+
+    }
+
+    return (found);
+}
+
+
 /* extra score for 5 or 6 bands for G4FOC */
 int g4foc_score() {
-    return 0;
+
+    int points = g4foc_count;
+    if (g4foc_count == 5)
+	points += 10;
+    else if (g4foc_count == 6)
+	points += 15;
+
+    return points;
 }
+
 
 
 /* count nr of countries worked on all bands */
@@ -147,24 +177,60 @@ int get_nr_cont() {
     return nr;
 }
 
+/** calculate total score for FOC marathon
+ *
+ * \return number of points
+ */
+int foc_totalscore() {
+    extern int call_band[];
 
-void foc_show_scoring(int start_column) {
     int points;
-    int cntry  = get_nr_cntry();
-    int cont   = get_nr_cont();
 
+    /* first find Gx4FOC in call array and see how often we worked him */
+    g4foc_index = search_g4foc_in_callarray();
+
+    if (g4foc_index != -1)
+	g4foc_count = nr_of_bands(call_band[g4foc_index]);
+    else
+	g4foc_count = 0;
+
+    /* count countries and continents */
+    cntry  = get_nr_cntry();
+    cont   = get_nr_cont();
+
+    /* count 5 and 6 banders and eventually correct for G4FOC */
     count_56_banders();
+    if (g4foc_count == 5)
+	five_banders--;
+    else if (g4foc_count == 6)
+	six_banders--;
 
-    points = total +
+    points = total - (2 * g4foc_count) +
 	five_banders * 10 +
 	six_banders * 15 +
 	cntry * 2 +
 	cont * 5 +
-	g4foc_score();
+	g4foc_score() * 2;
 
+    return points;
+}
+
+/** display scoring for FOC marathon */
+void foc_show_scoring(int start_column) {
+
+    int points = foc_totalscore();
+
+#ifdef old_format
     mvprintw(4, start_column, "Ctry: %3d  Cont: %1d  G4FOC: %c",
 	    cntry, cont, got_g4foc ? 'x' : '-');
     mvprintw(5, start_column, "  5b: %3d  6b: %3d  Score: %d",
 	    five_banders, six_banders, points);
+#else
+    mvprintw(4, start_column, "%s", "QSO  Cty Cont   5b    6b FOC  Score");
+    mvprintw(5, start_column, "%3d  %3d  %2d  %4d  %4d  %2d   %4d",
+	    total - (2 * g4foc_count), cntry * 2, cont * 5,
+	    five_banders * 10, six_banders * 15, 2 * g4foc_score(), points);
+#endif
+
 }
 
