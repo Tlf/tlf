4ef893a58f296a487fcbedf5db4beec962d2cf34
Ervin Hegedus <airween@gmail.com>
2014-09-13 23:33:30 +0200

    Aligned qtcmerge.py to new QTC log structure

--- share/qtcmerge.py
+++ share/qtcmerge.py
@@ -11,8 +11,8 @@ if len(sys.argv) < 2:
 cbr = sys.argv[1]
 sqtc = []
 rqtc = []
-sqtctimes = []
-rqtctimes = []
+sqtcserials = []
+rqtcserials = []
 
 band_hash = {
   160: "1800",
@@ -21,6 +21,11 @@ band_hash = {
    20: "14000",
    15: "21000",
    10: "28000"
+}
+
+mode_hash = {
+  'CW': "CW",
+  'SS': "PH"
 }
 
 try:
@@ -45,16 +50,21 @@ try:
 	fshift = 0
 	fields = filter(None, [fl for fl in l.split(" ")])
 
-	if len(fields) == 12:
+	if l[33] != " ":
 	    fshift = 1
-	qtc = ['QTC:', "%s" % band_hash[(int(l[0:3]))].rjust(5, ' '), l[3:5], time.strftime("%Y-%m-%d %H%M", time.strptime(fields[3] + " " + fields[4], "%d-%b-%y %H:%M")), fields[5+fshift].ljust(13, ' ')]
+
+	if fields[-1].strip() != "*":
+	    freq = fields[-1].split(".")[0].rjust(5, ' ')
+	else:
+	    freq = band_hash[(int(l[0:3]))].rjust(5, ' ')
+	qtc = ['QTC:', freq, mode_hash[l[3:5]], time.strftime("%Y-%m-%d %H%M", time.strptime(fields[3] + " " + fields[4], "%d-%b-%y %H:%M")), fields[5+fshift].ljust(13, ' ')]
 	qtc.append("%s" % ("%d/%d" % (int(fields[6+fshift]), int(fields[7+fshift]))).ljust(10, " "))
 	qtc.append("%s" % (callsign.ljust(13, ' ')))
 	qtc.append("%s" % (fields[8+fshift]))
 	qtc.append("%s" % (fields[9+fshift].ljust(13, ' ')))
 	qtc.append("%s" % (fields[10+fshift].strip()))
 	sqtc.append(" ".join(qtc))
-	sqtctimes.append(int(time.strftime("%s", time.strptime(fields[3] + " " + fields[4], "%d-%b-%y %H:%M"))))
+	sqtcserials.append(fields[1])
     f.close()
 except:
     print "Can't open or parse QTC_sent.log"
@@ -66,45 +76,40 @@ try:
 	fshift = 0
 	fields = filter(None, [fl for fl in l.split(" ")])
 
-	if len(fields) == 11:
+	if l[28] != " ":
 	    fshift = 1
-	qtc = ['QTC:', "%s" % band_hash[(int(l[0:3]))].rjust(5, ' '), l[3:5], time.strftime("%Y-%m-%d %H%M", time.strptime(fields[2] + " " + fields[3], "%d-%b-%y %H:%M")), "%s" % callsign.ljust(13, ' ')]
+	band = band_hash[(int(l[0:3]))].rjust(5, ' ')
+	if fields[-1].strip() != "*":
+	    freq = fields[-1].split(".")[0].rjust(5, ' ')
+	else:
+	    freq = band_hash[(int(l[0:3]))].rjust(5, ' ')
+	qtc = ['QTC:', freq, mode_hash[l[3:5]], time.strftime("%Y-%m-%d %H%M", time.strptime(fields[2] + " " + fields[3], "%d-%b-%y %H:%M")), "%s" % callsign.ljust(13, ' ')]
 	qtc.append("%s" % ("%d/%d" % (int(fields[5+fshift]), int(fields[6+fshift]))).ljust(10, " "))
 	qtc.append("%s" % (fields[4+fshift].ljust(13, ' ')))
 	qtc.append("%s" % (fields[7+fshift]))
 	qtc.append("%s" % (fields[8+fshift].ljust(13, ' ')))
 	qtc.append("%s" % (fields[9+fshift].strip()))
 	rqtc.append(" ".join(qtc))
-	rqtctimes.append(int(time.strftime("%s", time.strptime(fields[2] + " " + fields[3], "%d-%b-%y %H:%M"))))
+	rqtcserials.append(fields[1])
     f.close()
 except:
     print "Can't open or parse QTC_recv.log"
 
 fo = open(cbr.replace(".cbr", "_QTC.cbr"), "w+")
-pos, rpos, spos = 0, 0, 0
-for l in lines[:-1]:
-    f = filter(None, [fl for fl in l.split(" ")])
-    if len(f) > 1 and f[0] == "QSO:":
-	qsotime = int(time.strftime("%s", time.strptime(f[3] + " " + f[4], "%Y-%m-%d %H%M")))
+rpos, spos = 0, 0
+for l in range(len(lines)):
+    f = filter(None, [fl for fl in lines[l].split(" ")])
+    if len(f) < 1 or f[0] != "QSO:":
+	fo.write(lines[l])
     else:
-	qsotime = None
-    if qsotime is not None:
-	while len(rqtctimes) > rpos and rqtctimes[rpos] < qsotime:
+	fo.write(lines[l])
+	serial = f[7]
+	while len(rqtcserials) > rpos and serial == rqtcserials[rpos]:
 	    fo.write(rqtc[rpos] + "\n")
 	    rpos += 1
-	while len(sqtctimes) > spos and sqtctimes[spos] < qsotime:
+	while len(sqtcserials) > spos and serial == sqtcserials[spos]:
 	    fo.write(sqtc[spos] + "\n")
 	    spos += 1
-    fo.write(l)
-    pos += 1
-#if len(rqtc) > 0:
-#    for q in rqtc:
-#	fo.write(q + "\n")
-#if len(sqtc) > 0:
-#    for q in sqtc:
-#	fo.write(q + "\n")
-
-fo.write(lines[-1])
 
 fo.close()
 
