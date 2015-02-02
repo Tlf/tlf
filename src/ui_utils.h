7da3eefe253bb5f69cabc3fb28b1eda3f47d379f
Thomas Beierlein <tb@forth-ev.de>
2015-02-02 20:42:37 +0100

    Add files to provide User Interface utilities
    - Linux terminal requires A_BOLD to be set for most attributes.
      Instead of adding the check for use_rxvt to each attribute settings
      we provide modify_attr() function to do it instead.

---
+++ src/ui_utils.h
@@ -0,0 +1,26 @@
+/*
+ * Tlf - contest logging program for amateur radio operators
+ * Copyright (C) 2015 Thomas Beierlein <tb@forth-ev.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef UI_UTILS_H
+#define UI_UTILS_H
+
+int modify_attr( int attr );
+
+
+#endif /* UI_UTILS_H */
