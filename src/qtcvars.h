9d32cee8fcc0452d0237ea6370c8b7cc5f5aad9e
Ervin Hegedus <airween@gmail.com>
2015-04-07 22:09:13 +0200

    Added new header files, whith factorited QTC vars

---
+++ src/qtcvars.h
@@ -0,0 +1,37 @@
+/*
+ * Tlf - contest logging program for amateur radio operators
+ * Copyright (C) 2015 Ervin Hegedus <airween@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef QTCVARS_H
+#define QTCVARS_H
+
+extern int nr_qsosflags_for_qtc;	// number of lines in qsoflags_for_qtc[]
+extern int next_qtc_qso;		// the next non-sent QSO, which can be send next as QTC
+extern int qsoflags_for_qtc[MAX_QSOS];	// array of flag to log lines of QSOs for QTC's;
+					// this is an array of flags, which marks when a QSO sent as QTC
+extern int qtcdirection;		// 1: RECV, 2: SEND, 3: BOTH
+extern t_qtclist qtclist;		// the QTC list to send
+extern t_qtcreclist qtcreclist;		// the QTC list which received
+extern int nr_qtcsent;			// number of sent QTC
+extern t_qtc_ry_line qtc_ry_lines[QTC_RY_LINE_NR];	// when QTC is set, and mode is RTTY, then the modem lines stored this array
+extern int qtc_ry_currline;		// current line of QTC RTTY modem
+extern int qtc_ry_capture;		// enable or disable QTC RTTY capturing
+extern int qtc_ry_copied;		// stores the number of copied lines from QTC RTTY terminal to QTC window
+extern struct t_qtc_store_obj *qtc_temp_obj;		// temp object to help to maintain the QTC hash
+
+#endif /* end of include guard: QTCVARS_H */
