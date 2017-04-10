2a1b03e158ecfd8038db9a403913b0a8a55ae69d
Thomas Beierlein <tb@forth-ev.de>
2017-04-10 13:20:46 +0200

    factor out common code
    getctynr and getctydata uses the same code to prepare the call and look
    it up in prefix table.

--- src/getctydata.c
+++ src/getctydata.c
@@ -26,7 +26,7 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <malloc.h>
 #include "dxcc.h"
 #include "getpx.h"
 #include "globalvars.h"		// Includes glib.h and tlf.h
@@ -101,7 +101,14 @@ void change_area(char *call, char area){
 }
 
 
-int getpfxindex(char *checkcallptr)
+/* prepare and check callsign and look it up in dxcc data base
+ *
+ * returns index in data base or -1 if not found
+ * if normalized_call ptr is not NULL retruns a copy of the normalized call
+ * e.g. DL1XYZ/PA gives PA/DL1XYZ
+ * caller has to free the copy after use
+ */
+int getpfxindex(char *checkcallptr, char **normalized_call)
 {
     char checkcall[17] = "";
     char strippedcall[17] = "";
@@ -184,14 +191,18 @@ int getpfxindex(char *checkcallptr)
 	w = find_best_match( checkcall );
     }
 
+    if (normalized_call != NULL)
+	*normalized_call = strdup(checkcall);
+
     return w;
 }
 
+/* lookup dxcc cty number from callsign */
 int getctynr(char *checkcall)
 {
     int w;
 
-    w = getpfxindex(checkcall);
+    w = getpfxindex(checkcall, NULL);
 
     if (w >= 0)
 	return prefix_by_index(w)->dxcc_index;
@@ -200,94 +211,23 @@ int getctynr(char *checkcall)
 }
 
 
+/* lookup various dxcc cty data from callsign
+ *
+ * side effect: set up various global variables
+ */
 int getctydata(char *checkcallptr)
 {
-    char checkcall[17] = "";
-    char strippedcall[17] = "";
+    int w = 0, x = 0;
+    char *normalized_call = NULL;
 
-
-    int w = 0, x = 0, abnormal_call = 0;
-    size_t loc;
-
-    g_strlcpy(strippedcall, checkcallptr, 17);
-
-    if (strstr(strippedcall, "/QRP") ==
-	    (strippedcall + strlen(strippedcall) - 4))
-	/* drop QRP suffix */
-	strippedcall[strlen(strippedcall) - 4] = '\0';
-
-    /* go out if /MM, /AM or similar */
-    if (location_unknown(strippedcall))
-	strippedcall[0] = '\0';
-
-    strncpy(checkcall, strippedcall, 16);
-
-    loc = strcspn(checkcall, "/");
-
-    if (loc != strlen(checkcall)) {
-	char checkbuffer[17] = "";
-	char call1[17];
-	char call2[17];
-
-	strncpy(call1, checkcall, loc);		/* 1st part before '/' */
-	call1[loc] = '\0';
-	strcpy(call2, checkcall + loc + 1);	/* 2nd part after '/' */
-
-	if (strlen(call2) < strlen(call1)
-	    && strlen(call2) > 1) {
-	    sprintf(checkcall, "%s/%s", call2, call1);
-	    abnormal_call = 1;
-	    loc = strcspn(checkcall, "/");
-	}
-
-	if (loc > 3) {
-
-	    strncpy(checkbuffer, (checkcall + loc + 1),
-		    (strlen(checkcall) + 1) - loc);
-
-	    if (strlen(checkbuffer) == 1)
-		checkcall[loc] = '\0';
-	    loc = strcspn(checkcall, "/");
-	}
-
-	if (loc != strlen(checkcall)) {
-
-	    if (loc < 5)
-		checkcall[loc] = '\0';	/*  "PA/DJ0LN/P   */
-	    else {		/*  DJ0LN/P       */
-		strncpy(checkcall, checkcall, loc + 1);
-	    }
-	}
-
-	/* ------------------------------------------------------------ */
-
-	if ((strlen(checkbuffer) == 1) && isdigit(checkbuffer[0])) {	/*  /3 */
-
-	    change_area (checkcall, checkbuffer[0]);
-
-	} else if (strlen(checkbuffer) > 1)
-	    strcpy(checkcall, checkbuffer);
-
-    }
-
-    /* -------------check full call exceptions first...--------------------- */
-
-    if (abnormal_call == 1) {
-	w = find_full_match(strippedcall);
-    } else {
-	w = find_best_match( strippedcall );
-    }
-
-    if (w < 0 && 0 != strcmp(strippedcall, checkcall)) {
-	// only if not found in prefix full call exception list
-	w = find_best_match( checkcall );
-    }
-
+    w = getpfxindex(checkcallptr, &normalized_call);
 
     if (wpx == 1 || pfxmult == 1)
     	/* needed for wpx and other pfx contests */
-	getpx(checkcall);
+	getpx(normalized_call);
 
+    free (normalized_call);
+    normalized_call = NULL;
 
     if (w >= 0 ) {
 	x = prefix_by_index(w)->dxcc_index;
@@ -299,9 +239,6 @@ int getctydata(char *checkcallptr)
 	strcpy(zone_export, cqzone);
     else
 	strcpy(zone_export, ituzone);
-
-// w must be >0  tb 17feb2011
-//    strncpy(ituzone, ituarray[w], 2);
 
     countrynr = x;
     g_strlcpy(continent, dxcc_by_index(countrynr) -> continent , 3);
