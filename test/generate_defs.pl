954b0a90ab2e95acb32682d622ec5912ddc17749
zcsahok <ha5cqz@freemail.hu>
2018-01-31 17:29:24 +0000

    introduce test framework based on cmocka

---
+++ test/generate_defs.pl
@@ -0,0 +1,106 @@
+use strict;
+
+my %groups;
+
+#
+# scan test sources
+#
+
+for my $c (glob 'test_*.c') {
+
+    my $group_name = substr($c, 5, -2);
+
+    open(my $SRC, $c) or die;
+    while (<$SRC>) {
+        if (/\/\/ OBJECT (\S+)/) {
+            push @{$groups{$group_name}{OBJECTS}}, $1;
+            next;
+        }
+        if (/^((int|void) ((setup|teardown|test)(_\w+)?)\(void \*\*\w+\))/) {
+            ${$groups{$group_name}{FUNCTIONS}}{$3} = $1;
+            # check if it's a test function
+            if ($2 eq 'void') {
+                my $name =  substr($3, 5);
+                push @{$groups{$group_name}{TESTS}}, $name;
+            }
+            next;
+        }
+    } 
+    close($SRC);
+
+}
+
+#
+# genarate defs.am and sources for run's
+#
+
+open(my $AM, '> defs.am') or die;
+
+my $ts = scalar(localtime);
+print $AM "# generated on $ts\n\n";
+
+my $tests = '';
+for my $g (sort keys %groups) {
+    $tests .= " run_$g";
+}
+print $AM "TESTS =$tests\n\n";
+print $AM "check_PROGRAMS =$tests\n\n";
+
+for my $g (sort keys %groups) {
+    my $objects = join(' ',@{$groups{$g}{OBJECTS}});
+    print $AM <<EOT;
+run_${g}_SOURCES = run_${g}.c test_${g}.c data.c functions.c
+run_${g}_LDADD = $objects
+
+EOT
+
+    open(my $C, "> run_${g}.c") or die;
+    print $C <<EOT;
+// generated on $ts
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+EOT
+    for my $f (sort keys %{$groups{$g}{FUNCTIONS}}) {
+        print $C "${$groups{$g}{FUNCTIONS}}{$f};\n";
+    }
+
+    print $C <<EOT;
+
+int main(void) {
+    const struct CMUnitTest tests[] = {
+EOT
+    my ($setup, $teardown);
+
+    for my $t (@{$groups{$g}{TESTS}}) {
+        $setup = $teardown = 'NULL';
+        if (defined ${$groups{$g}{FUNCTIONS}}{"setup_$t"}) {
+            $setup = "setup_$t";
+        } elsif (defined ${$groups{$g}{FUNCTIONS}}{setup_default}) {
+            $setup = 'setup_default';
+        }
+        print $C "      cmocka_unit_test_setup_teardown(test_${t}, ${setup}, ${teardown}),\n";
+    }
+
+    $setup = $teardown = 'NULL';
+    if (defined ${$groups{$g}{FUNCTIONS}}{setup}) {
+        $setup = 'setup';
+    }
+    if (defined ${$groups{$g}{FUNCTIONS}}{teardown}) {
+        $teardown = 'teardown';
+    }
+
+    print $C <<EOT;
+    };
+
+    return cmocka_run_group_tests(tests, ${setup}, ${teardown});
+}
+EOT
+    close($C);
+}
+
+close($AM);
+
+
---
+++ test/test_zone_nr.c
@@ -0,0 +1,30 @@
+#include "test.h"
+
+#include "../src/zone_nr.h"
+
+// OBJECT ../src/zone_nr.o
+
+
+void test_ok_08(void **state) {
+    assert_int_equal(zone_nr("08"), 8);
+}
+
+void test_ok__8(void **state) {
+    assert_int_equal(zone_nr(" 8"), 8);
+}
+
+void test_ok_14(void **state) {
+    assert_int_equal(zone_nr("14"), 14);
+}
+
+void test_bad_empty(void **state) {
+    assert_int_equal(zone_nr(""), 0);
+}
+
+void test_bad_nan(void **state) {
+    assert_int_equal(zone_nr("foo"), 0);
+}
+
+//void test_bad_short(void **state) {
+//    assert_int_equal(zone_nr("7"), 0);
+//}
