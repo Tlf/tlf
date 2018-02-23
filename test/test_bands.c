43d0169dac428729452cad58f84f49c239ea96ce
Thomas Beierlein <tomjbe@gentoo.org>
2018-02-23 18:58:40 +0100

    extend test code for IsWarcIndex() and next_band()

--- test/test_bands.c
+++ test/test_bands.c
@@ -1,9 +1,11 @@
 #include "test.h"
 
 #include "../src/bands.h"
+#include "../src/globalvars.h"
 
 // OBJECT ../src/bands.o
 
+/* test conversion from bandindex to bandmask */
 void test_toBandMask(void **state) {
     assert_int_equal(inxes[BANDINDEX_160], BAND160);
     assert_int_equal(inxes[BANDINDEX_80], BAND80);
@@ -15,5 +17,43 @@ void test_toBandMask(void **state) {
     assert_int_equal(inxes[BANDINDEX_12], BAND12);
     assert_int_equal(inxes[BANDINDEX_10], BAND10);
     assert_int_equal(inxes[BANDINDEX_OOB], 0);
+}
+
+/* test IsWarcIndex */
+void test_IsWarcIndex(void **state) {
+    assert_int_equal(IsWarcIndex(BANDINDEX_160), 0);
+    assert_int_equal(IsWarcIndex(BANDINDEX_80), 0);
+    assert_int_equal(IsWarcIndex(BANDINDEX_40), 0);
+    assert_int_equal(IsWarcIndex(BANDINDEX_30), 1);
+    assert_int_equal(IsWarcIndex(BANDINDEX_20), 0);
+    assert_int_equal(IsWarcIndex(BANDINDEX_17), 1);
+    assert_int_equal(IsWarcIndex(BANDINDEX_15), 0);
+    assert_int_equal(IsWarcIndex(BANDINDEX_12), 1);
+    assert_int_equal(IsWarcIndex(BANDINDEX_10), 0);
+}
+
+/* test switch to next band UP or DOWN */
+void nextBandUpTest(void **state) {
+    bandinx = BANDINDEX_12;
+    next_band(BAND_UP);
+    assert_int_equal(bandinx, BANDINDEX_10);
+}
+
+void nextBandDownTest(void **state) {
+    bandinx = BANDINDEX_12;
+    next_band(BAND_DOWN);
+    assert_int_equal(bandinx, BANDINDEX_15);
+}
+
+void nextBandWrapUpwardsTest(void **state) {
+    bandinx = BANDINDEX_10;
+    next_band(BAND_UP);
+    assert_int_equal(bandinx, BANDINDEX_160);
+}
+
+void nextBandWrapDownwardsTest(void **state) {
+    bandinx = BANDINDEX_160;
+    next_band(BAND_DOWN);
+    assert_int_equal(bandinx, BANDINDEX_10);
 }
 
