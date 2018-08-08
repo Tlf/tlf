a5878a560933118f23107e0af6bff81d96ce9603
Stephen Hurd <shurd@sasktel.net>
2018-08-08 06:01:55 -0400

    POSIX doesn't define the -w option
    It seems to be a GNU extension, and doesn't seem to be needed in
    this use case.  The FreeBSD uniq utility doesn't support it.

--- src/readcalls.c
+++ src/readcalls.c
@@ -675,7 +675,7 @@ int synclog(char *synclogfile) {
 
     showmsg("Merging logfiles...\n");
     sleep(1);
-    sprintf(wgetcmd, "cat log1 log2 | sort -g -k4,4 | uniq -w79 > %s",
+    sprintf(wgetcmd, "cat log1 log2 | sort -g -k4,4 | uniq  > %s",
 	    logfile);
     if (system(wgetcmd) == 0)
 	showmsg("Merging logs successfull\n");
