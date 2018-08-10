420564503b0f4044bdcfef1eb6cd789fb554f3b7
Stephen Hurd <shurd@sasktel.net>
2018-08-09 20:08:57 -0400

    We can't use pragmas with Travis builds it seems.

--- src/ignore_unused.h
+++ src/ignore_unused.h
@@ -2,11 +2,8 @@
 #define IGNORE_UNUSED_H
 
 #define IGNORE(x) do { \
-	_Pragma("GCC diagnostic push");                            \
-	_Pragma("GCC diagnostic ignored \"-Wunknown-pragmas\"");    \
-	_Pragma("GCC diagnostic ignored \"-Wunused-result\"");       \
-	x;                                                            \
-	_Pragma("GCC diagnostic pop");                                 \
+	if (x);         \
+		(void)0; \
 } while(0)
 
 #endif
