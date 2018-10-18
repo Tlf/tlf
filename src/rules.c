4edc192687dc5e8e83aa51937cc893318a175984
zcsahok <ha5cqz@freemail.hu>
2018-10-18 19:32:13 +0000

    avoid redefining _GNU_SOURCE

--- src/rules.c
+++ src/rules.c
@@ -19,7 +19,10 @@
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
+#ifndef _GNU_SOURCE
 #define _GNU_SOURCE	// For asprintf()
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
