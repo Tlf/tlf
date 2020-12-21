9de048ab23a4bac207f959cb5b473deb947f1a0a
zcsahok <ha5cqz@freemail.hu>
2020-12-21 15:25:47 +0000

    fix warning in locator2longlat.c

--- src/locator2longlat.c
+++ src/locator2longlat.c
@@ -19,6 +19,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <string.h>
+
 /* returns true if 'qra' is a valid QRA locator
  * note: only the first 4 characters are tested
  */
