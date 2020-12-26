32ba2bf1bfc61b6f93d0bde9886036fc3f32b7b8
zcsahok <ha5cqz@freemail.hu>
2020-12-26 17:18:33 +0000

    use QRA for location if specified

--- src/locator2longlat.h
+++ src/locator2longlat.h
@@ -18,36 +18,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-/*
- * original code in HAMLIB
- *
- * src/locator.c header file
- *
- * author Stephane Fillod and the Hamlib Group
- * date 2000-2010
- *
- *  Hamlib Interface - locator, bearing, and conversion calls
- *
- *
- *  Hamlib Interface - locator and bearing conversion calls
- *  Copyright (c) 2001-2010 by Stephane Fillod
- *  Copyright (c) 2003 by Nate Bargmann
- *  Copyright (c) 2003 by Dave Hines
- *
- *
- *  Code to determine bearing and range was taken from the Great Circle,
- *  by S. R. Sampson, N5OWK.
- *  Ref: "Air Navigation", Air Force Manual 51-40, 1 February 1987
- *  Ref: "ARRL Satellite Experimenters Handbook", August 1990
- *
- *  Code to calculate distance and azimuth between two Maidenhead locators,
- *  taken from wwl, by IK0ZSN Mirko Caserta.
- *
- *  New bearing code added by N0NB was found at:
- *  http://williams.best.vwh.net/avform.htm#Crs
- */
-
-
 #ifndef LOCATOR2LONGLAT_H
 #define LOCATOR2LONGLAT_H
 
