302bb46bf3041525b7f00f63a1a37502f497b41e
Nate Bargmann <n0nb@n0nb.us>
2021-01-23 12:16:15 -0600

    Quell cppcheck warning in parse_logcfg.c
    While I don't really see this as necessarily improper code, I am adding
    initialization to the referenced variables to quell the cppcheck
    warnings.  Initialization is usually a good idea.
    
    Line 423:  Uninitialized variable: value
    Line 433:  Uninitialized variable: value
    Line 443:  Uninitialized variable: value
    Line 453:  Uninitialized variable: value
    Line 775:  Uninitialized variable: value
    Line 965:  Uninitialized variable: value

--- src/parse_logcfg.c
+++ src/parse_logcfg.c
@@ -415,7 +415,7 @@ static int cfg_bandmap(const cfg_arg_t arg) {
 }
 
 static int cfg_cwspeed(const cfg_arg_t arg) {
-    int value;
+    int value = 18;
     int rc = cfg_integer((cfg_arg_t) {.int_p = &value, .min = 6, .max = 60});
     if (rc != PARSE_OK) {
 	return rc;
@@ -425,7 +425,7 @@ static int cfg_cwspeed(const cfg_arg_t arg) {
 }
 
 static int cfg_cwtone(const cfg_arg_t arg) {
-    int value;
+    int value = 700;
     int rc = cfg_integer((cfg_arg_t) {.int_p = &value, .min = 0, .max = 999});
     if (rc != PARSE_OK) {
 	return rc;
@@ -435,7 +435,7 @@ static int cfg_cwtone(const cfg_arg_t arg) {
 }
 
 static int cfg_sunspots(const cfg_arg_t arg) {
-    int value;
+    int value = 0;
     int rc = cfg_integer((cfg_arg_t) {.int_p = &value, .min = 0, .max = 1000});
     if (rc != PARSE_OK) {
 	return rc;
@@ -445,7 +445,7 @@ static int cfg_sunspots(const cfg_arg_t arg) {
 }
 
 static int cfg_sfi(const cfg_arg_t arg) {
-    int value;
+    int value = 67;
     int rc = cfg_integer((cfg_arg_t) {.int_p = &value, .min = 0, .max = 1000});
     if (rc != PARSE_OK) {
 	return rc;
@@ -767,7 +767,7 @@ static int cfg_pfx_num_multis(const cfg_arg_t arg) {
 }
 
 static int cfg_sc_volume(const cfg_arg_t arg) {
-    int value;
+    int value = 0;
     int rc = cfg_integer((cfg_arg_t) {.int_p = &value, .min = 0, .max = 100});
     if (rc != PARSE_OK) {
 	return rc;
@@ -956,7 +956,7 @@ static int cfg_minitest(const cfg_arg_t arg) {
 	return PARSE_OK;
     }
 
-    int value;
+    int value = 60;
     int rc = cfg_integer((cfg_arg_t) {.int_p = &value, .min = 60, .max = 1800});
     if (rc != PARSE_OK) {
 	return rc;
