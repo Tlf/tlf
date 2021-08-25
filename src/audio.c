e26dfde01d75e7299cf97c0fefced8aa1ff3f297
Nate Bargmann <n0nb@n0nb.us>
2021-08-24 22:05:56 -0500

    Modify soundlog recording and playback
    * Allow for soundlog script in PATH
    
    As installation places the soundlog script into ${prefix}/bin,
    avoid requiring the user to copy it to the ~/tlf/soundlogs directory.
    
    It works just fine from PATH when called from inside the directory.
    
    * Query the environment for the value of HOME
    
    The opendir() function is not in a context where it understands
    the value of $HOME, unlike the system() calls used elsewhere which
    operate in a user's shell environment.  To make this work, query the
    environment for the value of HOME and concatenate it to the hard coded
    directory and pass that to opendir().
    
    Add some error handling so if in the future the directory is exposed to
    user configuration, the user won't be completely in the dark if there is
    a typo in the entered path.

--- src/audio.c
+++ src/audio.c
@@ -174,7 +174,7 @@ void record(void) {
 		IGNORE(system("echo " " > ~/.VRlock"));;
 
 		IGNORE(system
-		       ("cd ~/tlf/soundlogs; ./soundlog  > /dev/null 2> /dev/null &"));
+		       ("cd ~/tlf/soundlogs; soundlog  > /dev/null 2> /dev/null &"));
 
 		mvprintw(15, 20, "Contest recording enabled...");
 		refreshp();
@@ -194,10 +194,24 @@ void record(void) {
 
 	    // List contest recordings.
 	    case '3':
-		sounddir = opendir("$HOME/tlf/soundlogs/");	// (W9WI)
+		errno = 0;
 
-		if (sounddir == NULL)
+		/* Must query the environment for the value of $HOME
+		 * and build the path to the soundlogs.
+		 */
+		char *path = g_strdup_printf("%s%s",
+					     g_getenv("HOME"),
+					     "/tlf/soundlogs");
+
+		sounddir = opendir(path);
+
+		if (sounddir == NULL) {
+		    if (errno != 0) {
+			mvprintw(22, 1, "%s: %s", strerror(errno), path);
+			mvprintw(23, 1, "Press ESC to exit this screen");
+		    }
 		    break;
+		}
 
 		for (i = 4; i < 15; i++)
 		    mvprintw(i, 0,
@@ -223,6 +237,7 @@ void record(void) {
 			    i -= 10;
 		    }
 		}
+		g_free(path);
 		closedir(sounddir);
 
 	    // Play back contest recording.
