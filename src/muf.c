231694e359e199f92b95153597eb315d4bb60105
Thomas Beierlein <tomjbe@gentoo.org>
2023-10-19 21:00:57 +0200

    As C standards does not define M_PI or M_PI_2 provide a fallback

--- src/muf.c
+++ src/muf.c
@@ -32,6 +32,13 @@
 #include "tlf_panel.h"
 #include "ui_utils.h"
 
+#ifndef M_PI_2
+	# define M_PI_2         1.57079632679489661923  /* pi/2 */
+#endif
+
+#ifndef M_PI
+	# define M_PI           3.14159265358979323846  /* pi */
+#endif
 
 // message splitters:
 // line[0] - original line, content can be modified in-place
--- src/sunup.c
+++ src/sunup.c
@@ -23,6 +23,10 @@
 
 #include "get_time.h"
 
+#ifndef M_PI
+	# define M_PI           3.14159265358979323846  /* pi */
+#endif
+
 #define RADIAN  (180.0 / M_PI)
 
 
