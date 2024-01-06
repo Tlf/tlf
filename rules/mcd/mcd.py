727a7e60b697da9a3b933b079a260d5c2eb6acce
zcsahok <ha5cqz@gmx.com>
2024-01-06 19:27:12 +0000

    use initial exchanges for bandmap

--- rules/mcd/mcd.py
+++ rules/mcd/mcd.py
@@ -3,6 +3,23 @@ Marconi Club QSO Party Day
 http://www.ariloano.it/marconiclub/
 """
 
+import re
+
+MEMBERS = {}
+
+def init(cfg):
+    # read initial exchange file
+    global MEMBERS
+    comment = re.compile("(^#|^$)") # starts with a hash or empty
+    with open(cfg) as file:
+        for line in file:
+            line = line.strip()
+            if comment.match(line):
+                continue
+            [call, exchange] = line.split(',')
+            MEMBERS[call] = exchange
+
+
 def get_member_id(exchange):
     if exchange.startswith('MC'):
         return exchange
@@ -23,6 +40,9 @@ def score(qso):
 
 
 def check_exchange(qso):
+    mult = get_member_id(qso.exchange)
+    if not mult and qso.call in MEMBERS:
+        mult = MEMBERS[qso.call]
 
-    return {'mult1_value': get_member_id(qso.exchange)}
+    return {'mult1_value': mult}
 
