13df4ee8387b74ceba7b1647c0ad766a52a62c9d
Thomas Beierlein <tomjbe@gentoo.org>
2024-01-07 14:12:55 +0100

    give the user time to read the warning

--- src/main.c
+++ src/main.c
@@ -875,6 +875,7 @@ static void keyer_init() {
 	showmsg("CW-Keyer is Hamlib");
 	if (trx_control_disabled) {
 	    showmsg("Radio control disabled - no keying!");
+	    sleep(2);
 	    return;
 	}
 	if (!trx_control) {
