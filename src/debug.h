6f28cb0ee9fafbd7c7ae5ea939509063e985d9e4
Thomas Beierlein <tomjbe@gentoo.org>
2025-02-13 17:22:50 +0100

    Add TLF_LOG_... macros
    Just log the message into the debuglog file according to category

--- src/debug.h
+++ src/debug.h
@@ -33,6 +33,20 @@ enum debuglevel {
     TLF_DBG_DEBUG
 };
 
+/* Log Error, Warning, Info or Debug message to debuglog file if enabled */
+#define TLF_LOG_ERR(fmt, ...)  do { \
+	debug_log(TLF_DBG_ERR, fmt, ##__VA_ARGS__); \
+    } while(0)
+#define TLF_LOG_WARN(fmt, ...) do {\
+	debug_log(TLF_DBG_WARN, fmt, ##__VA_ARGS__); \
+    }while(0)
+#define TLF_LOG_INFO(fmt, ...) do {\
+	debug_log(TLF_DBG_INFO, fmt, ##__VA_ARGS__); \
+    }while(0)
+#define TLF_LOG_DEBUG(fmt, ...) do {\
+	debug_log(TLF_DBG_DEBUG, fmt, ##__VA_ARGS__); \
+    }while(0)
+
 bool debug_is_active();
 bool debug_init();
 void debug_log (enum debuglevel lvl,
