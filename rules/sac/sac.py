ed16628b5271103993d10e80b57d792804200fdf
zcsahok <ha5cqz@gmx.com>
2026-09-20 18:49:09 +0000

    support Scandinavian stations

--- rules/sac/sac.py
+++ rules/sac/sac.py
@@ -3,28 +3,46 @@ Scandinavian Activity Contest
 https://www.sactest.net/
 """
 
-SAC_PREFIXES = ['JW', 'JX', 'LA', 'OH', 'OH0', 'OJ0', 'OX', 'OY', 'OZ', 'SM', 'TF']
+SCANDINAVIAN_PREFIXES = {
+    'JW', 'JX', 'LA', 'OH', 'OH0', 'OJ0', 'OX', 'OY', 'OZ', 'SM', 'TF'
+}
 
 MY_CONTINENT = None
+SCANDINAVIAN_STATION = False
 
 def init(cfg):
     dxcc = tlf.get_dxcc(tlf.MY_CALL)
     global MY_CONTINENT
     MY_CONTINENT = dxcc.continent
+    SCANDINAVIAN_STATION = dxcc.main_prefix in SCANDINAVIAN_PREFIXES
 
 
 def score(qso):
     dxcc = tlf.get_dxcc(qso.call)
 
-    if dxcc.main_prefix not in SAC_PREFIXES:
-        return 0
+    # 7.1 For Scandinavian stations:
+    # EUROPEAN stations, outside Scandinavia, are worth two (2) points for every complete QSO.
+    # NON-EUROPEAN stations are worth three (3) points for every complete QSO.
+    if SCANDINAVIAN_STATION:
+        if dxcc.main_prefix in SCANDINAVIAN_PREFIXES:
+            return 0
 
-    points = 1
+        if dxcc.continent == 'EU':
+            points = 2
+        else:
+            points = 3
 
-    # NON-EUROPEAN stations receive three (3) points for every complete QSO
-    # on 3.5 and 7 MHz.
-    if MY_CONTINENT != 'EU' and qso.band >= 40:
-        points = 3
+    # 7.2 For non-Scandinavian stations:
+    # EUROPEAN stations receive one (1) point for every complete Scandinavian QSO.
+    # NON-EUROPEAN stations receive one (1) point for every complete Scandinavian QSO on 14, 21, and 28 MHz and three (3) points for every complete QSO on 3.5 and 7 MHz.
+    else:
+        if dxcc.main_prefix not in SCANDINAVIAN_PREFIXES:
+            return 0
+
+        points = 1
+
+        if MY_CONTINENT != 'EU' and qso.band >= 40:
+            points = 3
 
     return points
 
@@ -34,13 +52,21 @@ def check_exchange(qso):
 
     mult = ''
 
-    if dxcc.main_prefix in SAC_PREFIXES:
+    # 8.1 For Scandinavian stations
+    # Each worked DXCC entity is valid as one multiplier for each band.
+    if SCANDINAVIAN_STATION:
         mult = dxcc.main_prefix
-        if len(mult) == 2:
-            area = qso.call[2]
-            if not area.isdigit():
-                area = '0'
-            mult += area
+
+    # 8.2 For non-Scandinavian stations
+    # Each worked main prefix+number (0-9) in each Scandinavian DXCC entity.
+    else:
+        if dxcc.main_prefix in SCANDINAVIAN_PREFIXES:
+            mult = dxcc.main_prefix
+            if len(mult) == 2:
+                area = qso.call[2]
+                if not area.isdigit():
+                    area = '0'
+                mult += area
 
     return {'mult1_value': mult}
 
